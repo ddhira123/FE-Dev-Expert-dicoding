7685d4455df44da56e0235e8e40252e7bfa12d41
ddhira123 <dhana_dhira@apps.ipb.ac.id>
2021-07-18 16:41:55 +0700

    Change CACHE_NAME

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -3,7 +3,7 @@ const CONFIG = {
 	BASE_URL: 'https://restaurant-api.dicoding.dev/',
 	BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/',
 	DEFAULT_LANGUAGE: 'id',
-	CACHE_NAME: `RestoList-V1__${new Date().toISOString()}`,
+	CACHE_NAME: `RestoList-Cache__${new Date().toISOString()}`,
 	DATABASE_NAME: 'restolist-database',
 	DATABASE_VERSION: 1,
 	OBJECT_STORE_NAME: 'restaurants',
